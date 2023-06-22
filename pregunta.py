847bfb52b8996f5bc104da8be9b0dc56604699c9
Cesar <caospinamu@unal.edu.co>
2023-06-22 11:57:44 -0500

    Primer intento

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,59 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+import re
+from datetime import datetime
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";" , index_col=0)
 
+    # Delete the na values
+    df = df.dropna()
+
+    # Lowercase for the "sex" and "tipo_de_emprendimiento" columns
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+
+    # Lowercase for the "idea_negocio" column and remplace the "_" and "-" for " "
+    df["idea_negocio"] = df["idea_negocio"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.replace("_", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("-", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.strip()
+
+    # Same for "barrio"
+    df["barrio"] = df["barrio"].str.lower()
+    df["barrio"] = df["barrio"].str.replace("_", " ")
+    df["barrio"] = df["barrio"].str.replace("-", " ")
+    df["barrio"] = df["barrio"].str.strip()
+
+    # Convert the "estrato" and "comuna_ciudadano" column to int
+    df["estrato"] = df["estrato"].astype(int)
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    # Convert the "fecha_de_beneficio" column to datetime
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda fecha: '/'.join(fecha.split('/')[::-1]) if len(fecha.split('/')[0]) == 4 else fecha)
+
+    # Lowercase for the "monto_del_credito" column and remplace the " ", "$" for " " and "," for "" and convert to float
+    df['monto_del_credito'] = df.monto_del_credito.str.replace(' ', '')
+    df['monto_del_credito'] = df.monto_del_credito.str.replace('$', '')
+    df['monto_del_credito'] = df.monto_del_credito.str.replace(',', '')
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+
+    # Lowercase for the "línea_credito" column and remplace the "_" and "-" for " "
+    df['línea_credito'] = df.línea_credito.str.lower()
+    df['línea_credito'] = df.línea_credito.str.replace('_', ' ')
+    df['línea_credito'] = df.línea_credito.str.replace('-', ' ')
+
+    df.drop_duplicates(inplace=True)
+    
     #
     # Inserte su código aquí
     #
+    # print(df)
 
     return df
+
+
+clean_data()
